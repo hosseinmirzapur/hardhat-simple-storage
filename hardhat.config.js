2d679e0e8a4506dd61c6c2ee085204b1d7d4d7dd
Hossein Mirzapur <63141950+hosseinmirzapur@users.noreply.github.com>
2023-06-05 22:37:56 +0330

    deployment
    Deploying to test-net and local environment hardhat networks, using the hardhat.js library

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,21 @@
-require("@nomicfoundation/hardhat-toolbox");
+require("@nomicfoundation/hardhat-toolbox")
+require("dotenv").config()
+require("./tasks/block-number")
+
+const sepoliaConfig = {
+	url: process.env.SEPOLIA_URL,
+	accounts: [process.env.SEPOLIA_PRIVATE_KEY],
+	chainId: 11155111,
+}
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.18",
-};
+	solidity: "0.8.18",
+	networks: {
+		sepolia: sepoliaConfig,
+	},
+	defaultNetwork: "hardhat",
+	etherscan: {
+		apiKey: process.env.ETHERSCAN_API_KEY,
+	},
+}
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,32 +1,47 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
-const hre = require("hardhat");
+const { ethers, run, network } = require("hardhat")
 
-async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+const main = async () => {
+	console.clear()
+	const simpleStorageFactory = await ethers.getContractFactory("SimpleStorage")
+	console.log("Deploying contract...")
+	const simpleStorage = await simpleStorageFactory.deploy()
+	await simpleStorage.deployed()
+	console.log(`Deployed contract to: ${simpleStorage.address}...`)
 
-  const lockedAmount = hre.ethers.utils.parseEther("0.001");
+	if (network.name !== "hardhat") {
+		await simpleStorage.deployTransaction.wait(5)
+		await verify(simpleStorage.address, [])
+	}
 
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+	const currentValue = await simpleStorage.retrieve()
+	console.log(`Current Value is: ${currentValue}`)
 
-  await lock.deployed();
-
-  console.log(
-    `Lock with ${ethers.utils.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
+	// Update the current value
+	const transactionResponse = await simpleStorage.store(7)
+	await transactionResponse.wait(1)
+	const updatedValue = await simpleStorage.retrieve()
+	console.log(`Updated Value is: ${updatedValue}`)
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+const verify = async (contractAddress, args) => {
+	console.log("Verifying Contract...")
+	try {
+		await run("verify:verify", {
+			address: contractAddress,
+			constructorArguments: args,
+		})
+	} catch (error) {
+		if (error.message.toLowerCase().includes("already verified")) {
+			console.log("Already Verified!")
+		} else {
+			console.log(error)
+		}
+	}
+}
+
+main()
+	.then(() => process.exit(0))
+	.catch((error) => {
+		console.error(error)
+		process.exit(1)
+	})
